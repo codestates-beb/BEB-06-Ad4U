cf83346ad682e0bf2aeb9bb493b5ff2730706dae
홍찬우 <72719325+HCW-code@users.noreply.github.com>
2022-11-12 23:36:14 +0900

    Delete testRouter.js

--- server/router/testRouter.js
+++
@@ -1,9 +0,0 @@
-const { findAll, test } = require('../controller/test');
-const express = require('express');
-const router = express.Router();
-
-router.get('/', findAll);
-
-router.get('/test', test);
-
-module.exports = router;
