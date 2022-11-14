57b99f836a9070f5b940f23344e22b7b73c3fd25
Ellie-kang <kya6873@hanmail.net>
2022-11-14 17:26:47 +0900

    수정중

--- frontend/src/component/Nav.js
+++ frontend/src/component/Nav.js
@@ -1,8 +1,24 @@
 import React from 'react';
 
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { Link } from 'react-router-dom';
+import Logo from './Logo.png';
+
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Link to="/">
+          <img 
+          alt = "Ad4U logo" src = {Logo} className="nav_logo"/>
+        </Link>
+
+      </Container>
+    </Navbar>
+   
+
+    
   )
 }
 
