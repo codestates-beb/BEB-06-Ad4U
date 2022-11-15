efca060ccd253e40bc86d2ec2ed1c6288cfb867b
Ellie-kang <kya6873@hanmail.net>
2022-11-15 11:29:07 +0900

    Update files

--- frontend/src/component/Nav.js
+++ frontend/src/component/Nav.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Logo from './Logo.png';
 import './Nav.css';
 
-import { Navbar, NavDropdown, FormControl, Form, Button, Offcanvas } from 'react-bootstrap';
+import { Navbar, NavDropdown } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Stack from 'react-bootstrap/Stack';
 import Avatar from 'react-avatar';
@@ -11,6 +11,27 @@ import Login from './Login';
 import Signup from './Signup';
 
 const Nav = () => {
+  const [isLogin, setIsLogin] = useState(true);
+
+  const LoggedIn = () => {
+    return (
+      <Stack direction="horizontal" gap={4} justifyContent='flex-end'>
+        <Link to='/mypage/client'>
+          <Avatar size="50" round={true}/>
+        </Link>
+      </Stack>
+    )
+  }
+
+  const Logout = () => {
+    return (
+      <Stack direction="horizontal" gap={4} justifyContent='flex-end'>
+        <Login />
+        <Signup />
+      </Stack>
+    )
+  }
+
   return (
     <Navbar bg="light" expand="lg">
       <Container>
@@ -18,20 +39,15 @@ const Nav = () => {
           <img className="nav_logo" src = {Logo} alt = "Ad4U logo" width={100} height={70}/>
         </Link>
         <Stack direction="horizontal" gap={4} justifyContent='flex-end'>
-          <Login />
-          {/* <Button variant="light" className='Loginbtn'></Button>
-          <Button variant="light" className='signupbtn'>Signup</Button> */}
-          <Signup />
-          <Avatar facebookId="100008343750912" size="50" round={true}/>
+          {isLogin ? <LoggedIn /> : <Logout />}
           <NavDropdown id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <NavDropdown.Item href="/mypage/client">clientmypage</NavDropdown.Item>
+          <NavDropdown.Item href="/mypage/supplier">suppliermypage</NavDropdown.Item>
+          <NavDropdown.Item href="/list">list</NavDropdown.Item> 
           </NavDropdown>
         </Stack>
       </Container>
     </Navbar>
-    
   )
 }
 
