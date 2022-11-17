960a05eb34e0527026e3ce59b2a50eaa6fb78e04
apfl99 <rmrehd604@gmail.com>
2022-11-17 17:56:50 +0900

    add : baseUrl

--- client/src/pages/detail/component/AdUpload.js
+++ client/src/pages/detail/component/AdUpload.js
@@ -17,7 +17,7 @@ const AdUpload = () => {
     title : "",
     content : "",
     cost : "",
-    imgUrl : "",
+    imgUrl : "https://beb-project3-s3-bucket.s3.ap-northeast-2.amazonaws.com/favicon.png",
     isClient : true
   });
 
@@ -66,7 +66,29 @@ const AdUpload = () => {
       });
   }
 
-  const handleSubmit = async () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    //AdInfo 유효성 검사
+    var checkVaild = 0;
+    
+    if(AdInfo.content.length === 0) {
+        document.getElementById("content-message").style.display = "block";
+        checkVaild++;
+    }
+    if(AdInfo.cost.length === 0) {
+        document.getElementById("cost-message").style.display = "block";
+        checkVaild++;
+    }
+    if(AdInfo.title.length === 0) {
+        document.getElementById("title-message").style.display = "block";
+        checkVaild++;
+    }
+
+    if(checkVaild > 0) {
+        return;
+    }
+    
     const options = {
         url: "http://localhost:3001/ad/create",
         method: 'POST',
@@ -78,43 +100,50 @@ const AdUpload = () => {
             cost: AdInfo.cost,
             isClient: true
         }
-      }
-      axios.request(options)
+    }
+    axios.request(options)
         .then(res => {
-          if(res.status == 400) {
+        if(res.status == 400) {
 
-          }
-          else {
+        }
+        else {
             
-          }
+        }
         })
         .catch(err => console.log(err))
-    }
+
+  }
 
   
 
   return (
     <Form>
       <Form.Group controlId="formFile" className="mb-3">
-        <Form.Label>이미지 업로드</Form.Label>
+        <Form.Label><h5>이미지 업로드</h5></Form.Label>
         <Form.Control type="file" onChange={handleFileInput}/>
       </Form.Group>
+      
       <Form.Group className="mb-3" controlId="formAdTitle">
-        <Form.Label>광고 제목</Form.Label>
+        <Form.Label><h5>광고 제목<span className="red"> *</span></h5></Form.Label>
         <Form.Control type='text' placeholder="Enter Advertisement Title" onChange={handleAdTitle}/>
       </Form.Group>
+      <p id="title-message" className="titleMessage">Title Field Required!!</p>
+      
       <Form.Group>
-        <Form.Label>제안 금액</Form.Label>
+        <Form.Label><h5>제안 금액<span className="red"> *</span></h5></Form.Label>
         <InputGroup className="mb-3">
             <Form.Control type='text' placeholder="Enter Advertisement Cost" onChange={handleAdCost}/>
-            <InputGroup.Text id="basic-addon2">ETH</InputGroup.Text>
+            <InputGroup.Text>ETH</InputGroup.Text>
         </InputGroup>
+        <p id="cost-message" className="costMessage">Cost Field Required!!</p>
       </Form.Group>
+
       <Form.Group className="mb-3" controlId="formAdContent">
-        <Form.Label>광고 내용</Form.Label>
+        <Form.Label><h5>광고 내용<span className="red"> *</span></h5></Form.Label>
         <Form.Control as="textarea" placeholder="Enter Your Advertisement Info" rows={3} onChange={handleAdContent}/>
       </Form.Group>
-      
+      <p id="content-message" className="contentMessage">Content Field Required!!</p>
+
       <Button variant="primary" onClick={() => setModalShow(true)}>
         미리보기
       </Button>
