dfc273e66bd0b8078c04462a5fedc87b439887d3
HCW <jingi6052@naver.com>
2022-11-23 12:33:37 +0900

    mypage api fix

--- server/controller/users.js
+++ server/controller/users.js
@@ -239,7 +239,7 @@ module.exports = {
                                         include: [
                                             {
                                                 model: Supplier, as: "Supplier",
-                                                attributes: ['id', 'channelName', 'channelUrl', 'viewCount', 'subscriberCount', 'profileImgUrl'],
+                                                attributes: ['id', 'channelName', 'channelUrl', 'viewCount', 'subscriberCount', 'profileImgUrl', 'address'],
                                             }
                                         ],
                                     }
