668b7d945cffb2cebe5f531fe244529214e87eb5
yiminwook <grs0416@gmail.com>
2022-11-23 18:47:13 +0900

    Refactoring status

--- client/src/pages/mypage/component/Status.js
+++ client/src/pages/mypage/component/Status.js
@@ -1,18 +1,23 @@
-import React from 'react';
-import { Accordion, Col, Row, Container } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Col, Row, Container } from 'react-bootstrap';
 
 import './Status.css'
 
 const Status = ({ adList, setStatus }) => {
-  const count = (arr, status) => {
-    let result = 0;
-    arr.forEach(el => {
-      if(el.status === status) {
-        result++;
-      }
-    });
-    return result;
-  }
+  const [count, setCount] = useState({ status0: 0, status1: 0, status2: 0, status3: 0, status4: 0 })
+
+  useEffect(() => {
+    let status0 = 0, status1 = 0, status2 = 0, status3 = 0, status4 = 0;
+    adList.forEach(el => {
+      if (el.status === 0) return status0++;
+      if (el.status === 1) return status1++;
+      if (el.status === 2) return status2++;
+      if (el.status === 3) return status3++;
+      if (el.status === 4) return status4++;
+      else return ;
+    })
+    setCount({ status0, status1, status2, status3, status4 });
+  }, [adList]);
 
   return (
     <Container className='status_container'> 
@@ -27,11 +32,11 @@ const Status = ({ adList, setStatus }) => {
         </Row>
         <Row className='status_count'>
           <Col>{adList.length}</Col>
-          <Col>{count(adList, 0)}</Col>
-          <Col>{count(adList, 1)}</Col>
-          <Col>{count(adList, 2)}</Col>
-          <Col>{count(adList, 3)}</Col>
-          <Col>{count(adList, 4)}</Col>
+          <Col>{count.status0}</Col>
+          <Col>{count.status1}</Col>
+          <Col>{count.status2}</Col>
+          <Col>{count.status3}</Col>
+          <Col>{count.status4}</Col>
         </Row>
       </div>
     </Container>
