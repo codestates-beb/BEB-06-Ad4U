1d83b4c6852d2b5c2a97837a6c8e964c7289c72a
apfl99 <rmrehd604@gmail.com>
2022-11-24 00:25:56 +0900

    pdf decrypt & download

--- client/src/pages/mypage/component/SBTView/SBTView.js
+++ client/src/pages/mypage/component/SBTView/SBTView.js
@@ -2,18 +2,21 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './SBT.css';
 import { Row, Col, Card, Button, Container } from 'react-bootstrap';
+import Viewer from 'react-viewer';
 
 import nullImg from '../../../../dummyfiles/img1.png';
+import axios from 'axios';
+import crypto from 'crypto-js';
+import {triggerBase64Download} from 'common-base64-downloader-react';
 
 
 
 const SBTView = ({ userData, adList }) => {
   const [list, setList] = useState([]);
   const [sbtList, setSbtList] = useState([]);
+
+  const [base64PDF, setBase64PDF] = useState("");
   
-  // loading overlay
-  const [loaded, setLoaded] = useState(false);
-  console.log(adList)
 
   const navigate = useNavigate();
 
@@ -23,6 +26,35 @@ const SBTView = ({ userData, adList }) => {
     }));
     console.log(sbtList)
   }, [adList])
+
+  const dataURLtoBase64 = (dataurl) => {
+ 
+    var arr = dataurl.split(',')
+    
+    return arr[1];
+  }
+
+
+  const handleViewPdf = async (data) => {
+    const secretKey = process.env.REACT_APP_SECRET_KEY;
+    console.log(data)
+    console.log(data.token_uri)
+      const options = {
+        url: data.token_uri,
+        method: 'GET',
+        headers: {"Content-Type": "application/json"}
+      }
+      
+      await axios.request(options)
+      .then(res => {
+        console.log(res.data)
+        //복호화
+          const bytes = crypto.AES.decrypt(res.data, secretKey);
+          const decrypted = bytes.toString(crypto.enc.Utf8);
+          const decrypted_base64 = "data:application/pdf;base64,"+dataURLtoBase64(decrypted);
+          triggerBase64Download(decrypted_base64, `${data.title}_${data.createdAt}`)
+      })
+  }
 
   
 
@@ -34,7 +66,7 @@ const SBTView = ({ userData, adList }) => {
                 <div 
                 className="ad-content_card-container"
                 onClick = {() => {
-
+                  handleViewPdf(data)
                 }}
                 key={idx}
                 >
