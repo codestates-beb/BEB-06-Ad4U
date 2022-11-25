b50e887d5bd9429ca51a651feeaa05eb42a24110
yiminwook <grs0416@gmail.com>
2022-11-25 16:45:29 +0900

    Feat get client profile

--- server/controller/users.js
+++ server/controller/users.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const { Client, Supplier, Advertisement, Advertisement_has_Supplier } = require('../models/index');
 const jwt = require('jsonwebtoken');
-const client_attributes = ['id', 'userId', 'company_name', 'company_number', 'email'];
+const client_attributes = ['id', 'userId', 'company_name', 'company_number', 'email', 'profileImgUrl'];
 const supplier_attributes = ['id', 'userId', 'email', 'channelName', 'channelUrl', 'viewCount', 'subscriberCount', 'profileImgUrl', 'address'];
 const advertisement_attributes = ['id', 'title', 'AdimgUrl', 'cost', 'multisigAddress', 'token_id', 'token_address','token_uri', 'createdAt', 'status']
 const axios = require("axios");
