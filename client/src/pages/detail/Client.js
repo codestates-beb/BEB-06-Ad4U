356f3da7b83632d3c56f946ff5bc48f7ac6d5717
yiminwook <grs0416@gmail.com>
2022-11-25 15:41:47 +0900

    Fix Client DetailPage advertisements

--- client/src/pages/detail/Client.js
+++ client/src/pages/detail/Client.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import ad from '../../hooks/axios/ad';
 import client from '../../hooks/axios/client';
 import { useNavigate } from 'react-router-dom';
 
@@ -9,12 +10,25 @@ import './Detail.css';
 
 const ClientDetail = () => {
   const { clientId } = useParams();
-  const [detail, setDetail] = useState({ company_name: "", company_number: "", email: "", Advertisements:[] });
+  const [detail, setDetail] = useState({ company_name: "", company_number: "", email: "" });
+  const [advertisement, setAdvertisement] = useState([]);
   console.log("Detail", detail)
+  console.log("Advertisement", advertisement)
 
   useEffect(() => {
     client.getDetail(clientId)
-    .then(res=> setDetail(res.data))
+    .then(res => {
+      setDetail(res.data)
+      return ad.getList()
+      .then(res => res.data)
+      .then(data => {
+        let advertisements = [];
+        data.forEach(el => {
+          if(String(el.Client.id) === clientId) return advertisements.push(el);
+        });
+        setAdvertisement(advertisements);
+      })
+    })
     .catch(err => err.response.data)
   }, [clientId])
 
@@ -29,9 +43,24 @@ const ClientDetail = () => {
             <Card.Text>{detail.email}</Card.Text>
             <Card.Text>{detail.company_number}</Card.Text>
             <ListGroup.Item></ListGroup.Item>
-            <Card.Text as='h4' className='clientDetail_text'>진행중인 광고</Card.Text>
+            <Card.Text as='h4' className='clientDetail_text'>모집중인 광고</Card.Text>
             <div className="clientDetail-content">
-
+              {advertisement.length === 0 
+              ? <div>현재 모집중인 광고가 없습니다.</div>
+              : advertisement.map((el, idx) => {
+                console.log(el)
+                return (
+                  <div 
+                  className="clientDetail-content_card-container"
+                  onClick={() => navigate(`/detail/ad/${el.id}`)}
+                  key={idx}
+                  >
+                    {/* <div key={idx}> */}
+                      {/* <span>광고 # {idx+1}</span> */}
+                    <h5>{el.title}</h5>
+                    {/* </div> */}
+                  </div>
+              )})}
             </div>
           </ListGroup>
         </Card.Body>
@@ -43,17 +72,3 @@ const ClientDetail = () => {
 export default ClientDetail;
 
 
-// {detail.Advertisements.map((el, idx) => {
-//   console.log(el)
-//   return (
-//     <div 
-//     className="clientDetail-content_card-container"
-//     onClick={() => navigate(`/detail/ad/${el.id}`)}
-//     key={idx}
-//     >
-//       {/* <div key={idx}> */}
-//         {/* <span>광고 # {idx+1}</span> */}
-//       <h5>{el.title}</h5>
-//       {/* </div> */}
-//     </div>
-// )})}
